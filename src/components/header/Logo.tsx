d7a2e0da68986e0e8998f92a4d6e9d7ef024e0f0
mcc <markocalvocruz@gmail.com>
2025-01-06 18:39:36 -0800

    small fix

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 export const Logo = () => {
   return (
     <Link to="/" className="flex items-center gap-2 hover:opacity-80 transition-opacity">
-      <Swords className="w-6 h-6 text-gaming-primary" />
-      <span className="font-bold text-xl text-gaming-text-primary"></span>
+      <Swords className="w-6 h-6 text-gray" />
+      <span className="font-bold text-xl text-gaming-text-primary">ROI</span>
     </Link>
   );
 };
